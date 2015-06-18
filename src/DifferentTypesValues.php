08180e4dd552b61a332824328601a21c8fd37be5
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2015-06-18 23:13:18 +0200

    DifferentTypesValues::oneOfEachTypeProvider

---
+++ src/DifferentTypesValues.php
@@ -0,0 +1,63 @@
+<?php
+namespace Danwe\DataProviders;
+
+use LogicException;
+
+/**
+ * Provides a data provider returning different type's values.
+ *
+ * @since 1.0.0
+ * @author Daniel A. R. Werner
+ */
+class DifferentTypesValues {
+
+	/**
+	 * Returns a value of some type per case. Will analyse the name of the test using the provider.
+	 * If the test's name ends with "WithNon...Values" where "..." is the name of a type, e.g. "String",
+	 * "Null" or "Object", then no case for that type will be returned.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $testName
+	 * @return array( array( mixed $value, string $valuesType ), ... )
+	 *
+	 * @throws LogicException If the test function using this provider uses the "WithNon...Values"
+	 *         suffix where "..." is an unknown type.
+	 */
+	public static function oneOfEachTypeProvider( $testName = null ) {
+		$rawData = array( 'foo', 42, new \DateTime( '1980-01-01 1:00' ), 4.2, true, null, array() );
+		$cases = array();
+
+		foreach( $rawData as $data ) {
+			$dataType = gettype( $data );
+			$cases[ $dataType ] = array( $data, $dataType );
+		}
+
+		if( ! $testName ) {
+			return $cases;
+		}
+
+		preg_match(
+			'/^.+(?:W|_[Ww])ith(?:N|_[Nn])on((?:[A-Z]|_[a-zA-Z])[a-zA-Z]+)(?:V|_[Vv])alues$/',
+			$testName,
+			$matches
+		);
+
+		if( array_key_exists( 1, $matches ) ) {
+			$typeString = strtolower( ltrim( $matches[ 1 ], '_' ) );
+			if( $typeString === 'null' ) {
+				$typeString = 'NULL';
+			} else if( $typeString === 'float' ) {
+				$typeString = 'double'; // see http://php.net/manual/en/function.gettype.php
+			}
+
+			if( array_key_exists( $typeString, $cases ) ) {
+				unset( $cases[ $typeString ] );
+			} else {
+				throw new LogicException( __FUNCTION__ . " does not know of type \"$typeString\"");
+			}
+		}
+
+		return $cases;
+	}
+}
---
+++ tests/DifferentTypesValues_oneOfEachTypeProviderTest.php
@@ -0,0 +1,113 @@
+<?php
+namespace Danwe\DataProviders\Tests;
+
+use Danwe\DataProviders\DifferentTypesValues;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+class DifferentTypesValues_oneOfEachTypeProviderTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider functionNamesProvider
+	 */
+	public function testReturnsNonEmptyArray( $functionName ) {
+		$cases = DifferentTypesValues::oneOfEachTypeProvider( $functionName );
+
+		$this->assertInternalType( 'array', $cases );
+		$this->assertGreaterThan( 0, count( $cases ) );
+	}
+
+	public function testDoubleAndFloatAreSynonyms() {
+		$valuesDouble = DifferentTypesValues::oneOfEachTypeProvider( 'testWithNonDoubleValues' );
+		$valuesFloat = DifferentTypesValues::oneOfEachTypeProvider( 'testWithNonFloatValues' );
+
+		$this->assertEquals( $valuesDouble, $valuesFloat );
+	}
+
+	/**
+	 * @dataProvider functionNamesAndExcludedTypesProvider
+	 */
+	public function testReturnsExpectedValues( $functionName, array $excludedTypes ) {
+		$cases = DifferentTypesValues::oneOfEachTypeProvider( $functionName );
+
+		foreach( $cases as $i => $case ) {
+			list( $value, $valueType ) = $case;
+
+			$this->assertSame( gettype( $value ), $valueType,
+				'value is of type stated in 2nd parameter' );
+
+				$this->assertFalse( in_array( $valueType, $excludedTypes ),
+					"value with index $i returned with test function name set to "
+					. "\"$functionName\" is not of type(s): \""
+					. implode( '", "', $excludedTypes ) . '"' );
+		}
+	}
+
+	/**
+	 * @dataProvider functionNamesAndExcludedTypesProvider
+	 */
+	public function testReturnsOneOfEachIncludedType( $functionName, array $excludedTypes ) {
+		$types = [
+			'boolean', 'integer', 'double', 'string', 'array', 'object', 'NULL'
+		];
+		$coveredTypes = [];
+
+		$cases = DifferentTypesValues::oneOfEachTypeProvider( $functionName );
+
+		foreach( $cases as $i => $case ) {
+			list( $value, $valueType ) = $case;
+
+			$this->assertFalse( in_array( $valueType, $coveredTypes ),
+				"contains only one value of type \"$valueType\"" );
+
+			if( in_array( $valueType, $types ) ) {
+				$coveredTypes[] = $valueType;
+			} else {
+				$this->assertTrue( false, "contains value of unknown type \"$valueType\"" );
+			}
+
+		}
+		$this->assertTrue(
+			count( array_diff(
+				array_diff( $types, $excludedTypes ),
+				$coveredTypes
+			) ) === 0,
+			'contains one value of each included type'
+		);
+	}
+
+	/**
+	 * @return array( array( string $functionName, string[] $expectedExcludedTypes ), ... )
+	 */
+	public static function functionNamesAndExcludedTypesProvider() {
+		return array(
+			array( 'testWithNonBooleanValues', array( 'boolean' ) ),
+			array( 'testWithNonIntegerValues', array( 'integer' ) ),
+			array( 'testWithNonDoubleValues', array( 'double' ) ),
+			array( 'testWithNonFloatValues', array( 'double' ) ),
+			array( 'testWithNonStringValues', array( 'string' ) ),
+			array( 'testWithNonArrayValues', array( 'array' ) ),
+			array( 'test_with_non_array_values', array( 'array' ) ),
+			array( 'test_with_non_ARRAY_values', array( 'array' ) ),
+			array( 'test_With_non_Array_Values', array( 'array' ) ),
+			array( 'test_With_Non_Array_Values', array( 'array' ) ),
+			array( 'testWithNonARRAYValues', array( 'array' ) ),
+			array( 'testWithNonObjectValues', array( 'object' ) ),
+			array( 'testWithNonNullValues', array( 'NULL' ) ),
+			array( 'testWithNonNULLValues', array( 'NULL' ) ),
+		);
+	}
+
+	/**
+	 * @return array( array( string|null $functionName ), ... )
+	 */
+	public static function functionNamesProvider() {
+		$data = array_map( function( $value ) {
+			return [ $value[ 0 ] ];
+		}, static::functionNamesAndExcludedTypesProvider() );
+
+		$data[] = [ null ];
+
+		return $data;
+	}
+}
+
